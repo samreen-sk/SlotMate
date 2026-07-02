f904bb685299521926890c9c848af55833a39294
samreen-sk <samreensk555@gmail.com>
2026-07-02 22:00:35 +0530

    modifications in popup.js filee

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,49 @@
-const connectButton = document.getElementById("connectBtn");
+document.addEventListener("DOMContentLoaded", async () => {
 
-connectButton.addEventListener("click", () => {
-
-    chrome.storage.local.set({
-        autoBooking: true
-    }, () => {
-
-        alert("Auto Booking Enabled!");
-
+    const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
     });
+
+    chrome.tabs.sendMessage(
+        tab.id,
+        {
+            action: "GET_EXAMS"
+        },
+        (response) => {
+
+            const container =
+                document.getElementById("examList");
+
+            container.innerHTML = "";
+
+            if (!response || response.length === 0) {
+
+                container.innerHTML =
+                    "<p>No exams found.</p>";
+
+                return;
+            }
+
+            response.forEach((exam) => {
+
+                const div = document.createElement("div");
+
+                div.className = "exam-card";
+
+                div.innerHTML = `
+                    <h4>${exam.name}</h4>
+
+                    <p>${exam.time}</p>
+
+                    <p>Venue: ${exam.venue}</p>
+                `;
+
+                container.appendChild(div);
+
+            });
+
+        }
+    );
 
 });
